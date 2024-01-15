5e30c8eb1b8ff93bca2653428df5eaeac56a2b67
Sampreet <sampreetsingh0903@gmail.com>
2024-01-15 16:16:01 -0500

    Initial Commit

---
+++ DemoWeek2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoWeek2
+//
+//  Created by Sampreet singh on 15/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
