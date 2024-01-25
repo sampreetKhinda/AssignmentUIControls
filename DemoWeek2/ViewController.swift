1a256b23079bc25883edf9a7e98feb8351fe9aa1
Sampreet <sampreetsingh0903@gmail.com>
2024-01-25 12:22:30 -0500

    Initial commit for * Launch Screen * Main * View Controller

--- DemoWeek2/ViewController.swift
+++ DemoWeek2/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var i = 0
+    var steps = 1
+   
+    @IBOutlet weak var outputLabel: UILabel!
+    
+        @IBAction func decBtn(_ sender: UIButton) {
+            i-=steps
+            outputLabel.text = String(i)
+        }
+        @IBAction func incBtn(_ sender: UIButton) {
+            i+=steps
+            outputLabel.text = String(i)
+        }
+    
+    @IBAction func resetBtn(_ sender: Any) {
+        i = 0
+        steps = 1
+        outputLabel.text = String(i)
+    }
+    @IBAction func stepBtn(_ sender: Any) {
+        steps = 2
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
